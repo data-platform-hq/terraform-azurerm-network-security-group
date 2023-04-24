e403fcc306dd8eb1310817aee832eabc81282229
dmytro_velychko3 <you@example.com>
2023-04-24 11:09:13 +0300

    fix: output condition

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "id" {
-  value       = length(azurerm_network_security_group.this[0].id) != 0 ? azurerm_network_security_group.this[0].id : ""
+  value       = length(var.security_rules) != 0 ? azurerm_network_security_group.this[0].id : ""
   description = "The ID of the Network Security Group"
 }
